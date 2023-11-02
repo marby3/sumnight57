ccb615344a0ca371b3ae910e254d66199e84090c
marby3 <64513524+marby3@users.noreply.github.com>
2023-11-03 00:19:23 +0900

    fix

--- qmk_v0.22/keyboards/keyball/sumnight57/config.h
+++ qmk_v0.22/keyboards/keyball/sumnight57/config.h
@@ -28,8 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Split parameters
 #define SOFT_SERIAL_PIN         D2
-//#define MASTER_LEFT
-//#define SPLIT_HAND_MATRIX_GRID  B5, F6
+#define EE_HANDS
 #define SPLIT_USB_DETECT
 #define SPLIT_USB_TIMEOUT       500
 
